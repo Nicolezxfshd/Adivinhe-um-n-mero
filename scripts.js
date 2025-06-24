ad50d9bd2eaca44e970bef596782e0a67b7d82cb
Nicole Zimmermann <123264605+Nicolezxfshd@users.noreply.github.com>
2025-06-24 18:00:55 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+getelemementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+}
+random Number = Math.floor(Math.random() * 100) + 1;
+document.getElementById("randomNumberDisplay").innerText = "Random Number: " + randomNumber;
+document.getElementById("randomNumberDisplay").innerText = "Random Number: " + randomNumber;
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+});
+document.getElementById
